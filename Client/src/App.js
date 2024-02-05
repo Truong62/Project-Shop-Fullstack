23c32a0fb9f34fe0ae222e15e3ac5ca96852aa29
Truong62 <truong06022004@gmail.com>
2024-02-05 14:34:12 +0700

    update

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,6 +1,24 @@
+import { useEffect } from "react";
 import RouterPage from "./router/RouterPage";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    const activateSystem = async () => {
+      try {
+        await axios.get('https://nntshop.onrender.com/list-order');
+      } catch (error) {
+        console.error('Error activating system: ', error);
+      }
+    };
+
+    const fetchData = async () => {
+      await activateSystem();
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
       <RouterPage></RouterPage>
